f3284e0ff7a36abecfaa75912a4f57a905f80837
Tarje Sælen Lavik <Tarje.Lavik@ub.uib.no>
2019-10-17 09:21:25 +0200

    Add plugins for youtube, vimeo, csv and file

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -20,6 +20,44 @@ module.exports = {
         watchMode: !isProd,
         overlayDrafts: !isProd && token
       }
-    }
+    },
+    {
+      resolve: `gatsby-source-youtube-v2`,
+      options: {
+        channelId: ['UCsZUur3SucrmTiECYZvuLlw'],
+        apiKey: process.env.GOOGLE_YOUTUBE_API_TOKEN,
+        maxVideos: 500 // Defaults to 50
+      },
+    },
+    {
+      resolve: `gatsby-source-vimeo`,
+      options: {
+        clientID: process.env.VIMEO_CLIENTID,
+        clientSecret: process.env.VIMEO_TOKEN,
+        userID: 'uib',
+        // searchQuery: 'INSERT_SEARCH_QUERY [OPTIONAL]',
+        transformer (video) {
+          // Transform the video data [OPTIONAL]
+          // i.e. Add extra fields or alter existing field
+          video.newField = 'value'
+          return video
+        }
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data/`,
+      },
+    },
+    `gatsby-transformer-csv`,
   ]
 }
