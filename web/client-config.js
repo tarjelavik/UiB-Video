9dca3448424126b201cf0a57918243ef576f8159
Tarje Lavik <tarje.lavik@gmail.com>
2019-10-23 16:40:10 +0200

    Use first sanity project

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || '0vjt5fqa',
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || '7yfl1hym',
     dataset: process.env.GATSBY_SANITY_DATASET || 'production'
   }
 }
